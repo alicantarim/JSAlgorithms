119cd69be9627f32459f8b70c1efc754c02afaa6
Alican Tarım <alicantarim@hotmail.com>
2025-01-17 10:11:35 +0300

    Add: Comments for longest increasing function

--- longestIncreasingSubsequence.js
+++ longestIncreasingSubsequence.js
@@ -44,3 +44,16 @@ function longestIncreasingSubsequence(nums) {
 const input = [10, 9, 2, 5, 3, 7, 101, 18];
 const output = longestIncreasingSubsequence(input);
 console.log(output); // [2, 5, 7, 101]
+
+/* //! AÇIKLAMALAR
+1- dp Dizisi:
+        dp[i] her eleman için, o elemanda sonlanan en uzun artan alt dizinin uzunluğunu tutar. İlk başta, her elemanın kendi başına bir dizi olduğu varsayıldığı için 1 ile doldurulur.
+2- parent Dizisi:
+        Her eleman için, artan alt diziyi oluşturmak amacıyla, bir önceki elemanın indeksini saklar. Bu sayede, alt dizi geriye doğru takip edilerek oluşturulabilir.
+3- İç İçe Döngüler:
+        i ve j döngüleri, nums[i] değerine kadar olan tüm elemanlarla karşılaştırma yapar. Eğer nums[j] < nums[i] ve dp[j] + 1 > dp[i] ise, dp[i] ve parent[i] güncellenir.
+4- Maksimum Uzunluğu ve İndeksi Takip:
+        Döngü sırasında, en uzun alt dizinin uzunluğu (maxLength) ve sonlandığı indeks (maxIndex) sürekli takip edilir.
+5- Sonucu Oluşturma:
+        parent dizisini takip ederek, en uzun artan alt diziyi baştan sona oluştururuz. unshift fonksiyonu kullanılarak elemanlar başa eklenir.
+*/
